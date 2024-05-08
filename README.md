6a51fd3bd75d88841c80ec868b91ea7d0e7250a7
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-05-09 00:41:47 +0530

    DEVOPS-65 updated readme and fixed code

--- README.md
+++ README.md
@@ -1,20 +1,51 @@
 # publish-confluence-pages-programatically
 publish confluce pages programattically
 
-``-----------------------------------------------------------``
 # using poetry as python virtual environment manager
 
 * [Python - Poetry docs](https://python-poetry.org/docs/managing-environments/)
 * [Poetry set up - Pycharm](https://www.jetbrains.com/help/pycharm/poetry.html)
 
-## This is a test
+# Python sdk from atlassian
+* [Atlassian python sdk](https://atlassian-python-api.readthedocs.io/confluence.htmlhttps://atlassian-python-api.readthedocs.io/confluence.html)
 
-![Adding Images in Markdown](Screenshot.png)
+**Python-dotenv package takes care of env variable set up on local system**
 
-### This is another teST
+## .env file
+```
+CONFLUENCE_URL="xxxx"
+CONFLUENCE_USERNAME="xxxx"
+CONFLUENCE_PASSWORD="xxxx"
+```
 
-**Hello My dear**
+:heavy_check_mark:  Replace "xxxx" with proper values. this is used for authentication
 
-_hello_
+# Program Inputs
 
-`*************************************************`
+The program requires 4 Inputs
+
+* action --> action to be performed. One among 'create','update','append','delete'
+* confluence_space --> confluence space
+* confluence_page_title --> new confluence page title
+* markdown_file_path --> markdown file path and name based. of md files are inside a folder use complete path<br> </br>
+**Eg**: foldername/markdown file name
+
+# Run code in local
+
+1. clone the repository to your local
+2. Change directory to the cloned repository
+3. set up the .env file 
+4. Run `python3 confluence.py --confluence_space <provide your confluence space name> --confluence_page_title "<Confluence title>" --action <action> --markdown_file_path <markfown file>`
+5. Run python3 confluence.py --confluence_space devwithkrishna --confluence_page_title "This is the title - QT" --action update --markdown_file_path README.md
+
+
+`usage: confluence.py [-h] --action {create,update,append,delete}
+                     --confluence_space CONFLUENCE_SPACE
+                     --confluence_page_title CONFLUENCE_PAGE_TITLE
+                     --markdown_file_path MARKDOWN_FILE_PATH`
+
+
+>[!NOTE]
+> This Program uses username ad api key for authentication with atlassian confluence.
+> On local testing python-dotenv with .env file is used
+> On GitHub Workflow, passed as aa environment variable in from GitHub Secrets.
--- confluence.py
+++ confluence.py
@@ -1,22 +1,18 @@
 import argparse
 import os
 import markdown
+import datetime
 from atlassian import Confluence
 from dotenv import load_dotenv
 
-
-def confluence_client(confluence_url: str, username: str, password: str):
+def report_datetime():
     """
-    Create confluence client for authentication
+    python code to get time in format str
     :return:
     """
-
-    confluence_client = Confluence(
-        url=confluence_url,
-        username=username,
-        password=password,
-        cloud=True)
-    return confluence_client
+    now = datetime.datetime.now()
+    formatted_datetime = now.strftime('%d-%B-%Y %A %H:%M')
+    return formatted_datetime
 
 
 def get_confluence_page_id(confluence_client, confluence_space: str, confluence_page_title: str):
@@ -25,8 +21,8 @@ def get_confluence_page_id(confluence_client, confluence_space: str, confluence_
     :return:
     """
     confluence = confluence_client
-    confluence_page_id = confluence.get_page_id(space=confluence_space,
-                                                title=confluence_page_title)
+    confluence_page_id = confluence.get_page_id(space=confluence_space,title=confluence_page_title)
+    print(f'Confluence id is : {confluence_page_id}')
     return confluence_page_id
 
 
@@ -46,18 +42,17 @@ def render_html_from_md_file(markdown_file_path: str):
 
 def create_confluence_page(confluence_client, html_content, confluence_space: str, confluence_page_title: str):
     """
-    Create a confluence page using python sdk
+    Create a confluence page from scratch using python sdk
     :return:
     """
     confluence = confluence_client
-
     status = confluence.create_page(
         space=confluence_space,
         title=confluence_page_title,
         body=html_content
     )
-
-    print(status)
+    time = report_datetime()
+    print(f'Confluence page created on {confluence_space} with title {confluence_page_title} at {time}')
 
 
 def update_confluence_page_if_exists(confluence_client, html_content, confluence_page_id: str, confluence_page_title: str):
@@ -75,7 +70,9 @@ def update_confluence_page_if_exists(confluence_client, html_content, confluence
         title=confluence_page_title,
         body=html_content
     )
-    print(status)
+    time = report_datetime()
+    # print(status)
+    print(f'Confluence page updated on {confluence_page_title} at {time}')
 
 
 def append_body_to_page(confluence_client, html_content, confluence_page_id: str, confluence_page_title:str):
@@ -89,38 +86,91 @@ def append_body_to_page(confluence_client, html_content, confluence_page_id: str
         title=confluence_page_title,
         append_body=html_content
     )
-    print(status)
+    # print(status)
+    time = report_datetime()
+    print(f'Confluence page data appended on {confluence_page_title} at {time}')
+
+def delete_confluence_page(confluence_client, confluence_page_id: str):
+    """
+    Remove conluence page with confluence page of==id
+    :param ce_client:
+    :param confluence_page_id:
+    :return:
+    """
+    confluence = confluence_client
+    confluence.remove_page(page_id=confluence_page_id)
+    time = report_datetime()
+    print(f'Confluence page with id {confluence_page_id} removed at {time}')
+
+
+def choose_action(action:str,confluence_space:str,confluence_page_title:str,markdown_file_path):
+    """
+    choose a function based on action
+    :param action:
+    :param confluence_space:
+    :param confluence_page_title:
+    :param markdown_file_path:
+    :return:
+    """
+    load_dotenv()
+    confluence_url = os.getenv('CONFLUENCE_URL')
+    # print(confluence_url)
+    username = os.getenv('CONFLUENCE_USERNAME')
+    # print(username)
+    password = os.getenv('CONFLUENCE_PASSWORD')
+    # print(password)
+    # Create conflience client
+    confluence_client = Confluence(url=confluence_url, username=username,
+            password=password, cloud=True)
+    # Render markdon --> html
+    html_content = render_html_from_md_file(markdown_file_path=markdown_file_path)
+    if action == 'create':
+        print(f'Creating confluence page with title {confluence_page_title}...')
+        create_confluence_page(confluence_client=confluence_client, html_content=html_content,
+                               confluence_page_title=confluence_page_title, confluence_space=confluence_space)
+    elif action == 'update':
+        print(f'updating {confluence_page_title}...')
+        print(f'retreiving confluence page id...')
+        confluence_page_id = get_confluence_page_id(confluence_client=confluence_client, confluence_space=confluence_space,
+                                                    confluence_page_title=confluence_page_title)
+        update_confluence_page_if_exists(confluence_client=confluence_client, confluence_page_id=confluence_page_id,
+                                         html_content=html_content, confluence_page_title=confluence_page_title)
+    elif action == "append":
+        print(f'modifying confluence page...')
+        print(f'retreiving confluence page id...')
+        confluence_page_id = get_confluence_page_id(confluence_client=confluence_client,
+                                                    confluence_space=confluence_space,
+                                                    confluence_page_title=confluence_page_title)
+        append_body_to_page(confluence_client=confluence_client, confluence_page_title=confluence_page_title,
+                            confluence_page_id=confluence_page_id, html_content=html_content)
+    else:
+        print(f'Deleting confluence page with title {confluence_page_title}...')
+        print(f'retreiving confluence page id...')
+        confluence_page_id = get_confluence_page_id(confluence_client=confluence_client,
+                                                    confluence_space=confluence_space,
+                                                    confluence_page_title=confluence_page_title)
+        delete_confluence_page(confluence_client=confluence_client,confluence_page_id=confluence_page_id)
+
 
 def main():
     """ To test the script"""
     parser = argparse.ArgumentParser(description='Process arguments for the program')
+    parser.add_argument("--action", type=str, choices=['create','update','append','delete'], help='Possble actions', required=True)
     parser.add_argument("--confluence_space", type=str, help='Confluencce space', required=True)
     parser.add_argument("--confluence_page_title", type=str, help='Confluence page title', required=True)
     parser.add_argument("--markdown_file_path", type=str, help='Markdown file path with file name', required=True)
 
-    load_dotenv()
-    confluence_url = os.getenv('CONFLUENCE_URL')
-    print(confluence_url)
-    usernname = os.getenv('CONFLUENCE_USERNAME')
-    print(usernname)
-    password = os.getenv('CONFLUENCE_PASSWORD')
-    print(password)
 
     args = parser.parse_args()
+    action = args.action
     confluence_space = args.confluence_space
     confluence_page_title = args.confluence_page_title
     markdown_file_path = args.markdown_file_path
 
     # function call
-    confluence = confluence_client(confluence_url=confluence_url, username=usernname, password=password)
-    html_content = render_html_from_md_file(markdown_file_path=markdown_file_path)
-    # create_confluence_page(confluence_client=confluence, html_content=html_content)
-    confluence_page_id = get_confluence_page_id(confluence_client=confluence, confluence_space=confluence_space,
-                                                confluence_page_title=confluence_page_title)
-    # update_confluence_page_if_exists(confluence_client=confluence, confluence_page_id=confluence_page_id,
-    #                        html_content=html_content,confluence_page_title=confluence_page_title)
-    # append_body_to_page(confluence_client=confluence,confluence_page_title=confluence_page_title,
-    #                     confluence_page_id=confluence_page_id,html_content=html_content)
+    choose_action(action=action,confluence_space=confluence_space,
+                  confluence_page_title=confluence_page_title,markdown_file_path=markdown_file_path)
+
 
 if __name__ == "__main__":
     main()
